2ca415a47d059f3ed616d9101d75a83cd2b2d3d3
Gabriel Medeiros <gabrielmedeirosinacio@gmail.com>
2023-08-14 01:48:03 -0300

    refactor: dashboard

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,15 +6,13 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
+                        Mudar empresa
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link href="{{ route('posts.index') }}">
-                        Posts
-                    </x-nav-link>
+
                 </div>
             </div>
 
